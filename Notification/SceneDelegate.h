3d129dadf3f89828f7d0790bed96f504313e8d25
zhangguobing <zhangguobing@xindong.com>
2021-03-01 10:49:37 +0800

    Initial Commit

---
+++ Notification/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Notification
+//
+//  Created by spr on 2021/3/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Notification/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Notification
+//
+//  Created by spr on 2021/3/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
